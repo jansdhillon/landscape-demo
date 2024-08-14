1b2c37c8b882ac0cd5cf9df928d09666a4ac79e4
Rajan Patel <rajanpatel@responsiveweb.com>
2024-08-13 23:54:54 -0400

    updates for readability

--- add.sh
+++ add.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-today=$(date +%a%H%M | tr '[:upper:]' '[:lower:]')
+TODAY=$(date +%a%H%M | tr '[:upper:]' '[:lower:]')
 
 # Extract values from variables.txt
 HOSTNAME=$(grep '^HOSTNAME=' variables.txt | cut -d'=' -f2 | tr -d '[:space:]')
@@ -11,13 +11,13 @@ SSL_CERTIFICATE_KEY_PATH=$(grep '^SSL_CERTIFICATE_KEY_PATH=' variables.txt | cut
 
 # Construct Landscape FQDN based on available values
 if [ -n "$HOSTNAME" ] && [ -n "$DOMAIN" ]; then
-  landscape_fqdn="${HOSTNAME}.${DOMAIN}"
+  LANDSCAPE_FQDN="${HOSTNAME}.${DOMAIN}"
 elif [ -n "$HOSTNAME" ]; then
-  landscape_fqdn="${HOSTNAME}"
+  LANDSCAPE_FQDN="${HOSTNAME}"
 elif [ -n "$DOMAIN" ]; then
-  landscape_fqdn="${DOMAIN}"
+  LANDSCAPE_FQDN="${DOMAIN}"
 else
-  landscape_fqdn="landscape.example.com"
+  LANDSCAPE_FQDN="landscape.example.com"
 fi
 
 # Provision Landscape Server into LXD Container
@@ -25,7 +25,8 @@ fi
 # get Landscape Server cloud-init.yaml template
 curl -o cloud-init.yaml https://raw.githubusercontent.com/canonical/landscape-scripts/main/provisioning/cloud-init-quickstart.yaml
 # populate the template with information from variables.txt
-while IFS='=' read -r key value; do sed -i "s|{% set $key = '.*' %}|{% set $key = '$value' %}|" cloud-init.yaml; done < variables.txt
+while IFS='=' read -r KEY VALUE; do sed -i "s|{% set $KEY = '.*' %}|{% set $KEY = '$VALUE' %}|" cloud-init.yaml; done < variables.txt
+
 # check for optional SSL configurations
 if [[ -n "$SSL_CERTIFICATE_PATH" ]]; then
   SSL_CERTIFICATE=$(sudo awk '{print "    " $0}' "$SSL_CERTIFICATE_PATH" | sed ':a;N;$!ba;s/\n/\\n/g')
@@ -35,37 +36,39 @@ if [[ -n "$SSL_CERTIFICATE_KEY_PATH" ]]; then
   SSL_CERTIFICATE_KEY=$(sudo awk '{print "    " $0}' "$SSL_CERTIFICATE_KEY_PATH" | sed ':a;N;$!ba;s/\n/\\n/g')
   sed -i "/# - SSL_CERTIFICATE_KEY_FILE/a \\  - | \\n    cat <<EOF > /etc/ssl/private/landscape_server.key\\n${SSL_CERTIFICATE_KEY}\\n    EOF" cloud-init.yaml
 fi
+
 SSL_CERTIFICATE_CHAIN_PATH=$(grep '^SSL_CERTIFICATE_CHAIN_PATH=' variables.txt | cut -d'=' -f2)
 if [[ -n "$SSL_CERTIFICATE_CHAIN_PATH" ]]; then
   SSL_CERTIFICATE_CHAIN=$(sudo awk '{print "    " $0}' "$SSL_CERTIFICATE_CHAIN_PATH" | sed ':a;N;$!ba;s/\n/\\n/g')
   sed -i "/# - SSL_CERTIFICATE_CHAIN_FILE/a \\  - | \\n    cat <<EOF > /etc/ssl/certs/landscape_server_ca.crt\\n${SSL_CERTIFICATE_CHAIN}\\n    EOF" cloud-init.yaml
 fi
-## launch Noble instance with the Landscape Server cloud-init.yaml
-instance_name="$today-${landscape_fqdn//./-}"
-sudo sed -i "/$landscape_fqdn/d" /etc/hosts
-lxc launch ubuntu:24.04 "$instance_name" --config=user.user-data="$(cat cloud-init.yaml)"
-lxc exec "$instance_name" -- cloud-init status --wait
-# while [ -z "$LANDSCAPE_IP" ]; do LANDSCAPE_IP=$(lxc info "$instance_name" | grep -E 'inet:.*global' | awk '{print $2}' | cut -d/ -f1); [ -z "$LANDSCAPE_IP" ] && sleep 1; done
-LANDSCAPE_IP=$(lxc info "$instance_name" | grep -E 'inet:.*global' | awk '{print $2}' | cut -d/ -f1)
-echo "$LANDSCAPE_IP $landscape_fqdn" | sudo tee -a /etc/hosts > /dev/null
-for PORT in 6554 443 80; do lxc config device add "$instance_name" tcp${PORT}proxyv4 proxy listen=tcp:0.0.0.0:${PORT} connect=tcp:${LANDSCAPE_IP}:${PORT}; done
 
-echo "Visit https://$landscape_fqdn to finalize Landscape Server configuration,"
+# Launch Noble instance with the Landscape Server cloud-init.yaml
+INSTANCE_NAME="$TODAY-${LANDSCAPE_FQDN//./-}"
+sudo sed -i "/$LANDSCAPE_FQDN/d" /etc/hosts
+lxc launch ubuntu:24.04 "$INSTANCE_NAME" --config=user.user-data="$(cat cloud-init.yaml)"
+lxc exec "$INSTANCE_NAME" -- cloud-init status --wait
+LANDSCAPE_IP=$(lxc info "$INSTANCE_NAME" | grep -E 'inet:.*global' | awk '{print $2}' | cut -d/ -f1)
+echo "$LANDSCAPE_IP $LANDSCAPE_FQDN" | sudo tee -a /etc/hosts > /dev/null
+
+for PORT in 6554 443 80; do lxc config device add "$INSTANCE_NAME" tcp${PORT}proxyv4 proxy listen=tcp:0.0.0.0:${PORT} connect=tcp:"${LANDSCAPE_IP}":${PORT}; done
+
+echo "Visit https://$LANDSCAPE_FQDN to finalize Landscape Server configuration,"
 read -r -p "then press Enter to continue provisioning Ubuntu instances..."
 
-token="$(grep '^TOKEN=' variables.txt | cut -d'=' -f2)" # FROM ubuntu.com/pro/dashboard
-landscape_account_name="standalone"
-http_proxy=""
-https_proxy=""
-script_users="ALL"
-tags=""
-access_group="global"
-registration_key=""
+TOKEN="$(grep '^TOKEN=' variables.txt | cut -d'=' -f2)" # FROM ubuntu.com/pro/dashboard
+LANDSCAPE_ACCOUNT_NAME="standalone"
+HTTP_PROXY=""
+HTTPS_PROXY=""
+SCRIPT_USERS="ALL"
+TAGS=""
+ACCESS_GROUP="global"
+REGISTRATION_KEY=""
 
 # Determine if CERTBOT is set or both SSL_CERTIFICATE_FILE and SSL_CERTIFICATE_KEY_FILE are set
 if [[ -n "$CERTBOT" || (-n "$SSL_CERTIFICATE_FILE" && -n "$SSL_CERTIFICATE_KEY_FILE") ]]; then
     # If CERTBOT is set, or both SSL_CERTIFICATE_FILE and SSL_CERTIFICATE_KEY_FILE are set, use the valid SSL configuration
-cloud_init=$(cat <<EOF
+CLOUD_INIT=$(cat <<EOF
 #cloud-config
 packages:
   - ansible
@@ -78,14 +81,14 @@ runcmd:
   - systemctl stop unattended-upgrades
   - systemctl disable unattended-upgrades
   - apt-get remove -y unattended-upgrades
-  - pro attach $token
-  - landscape-config --silent --account-name="$landscape_account_name" --computer-title="\$(hostname --long)" --url "https://$landscape_fqdn/message-system" --ping-url "http://$landscape_fqdn/ping" --tags="$tags" --script-users="$script_users" --http-proxy="$http_proxy" --https-proxy="$https_proxy" --access-group="$access_group" --registration-key="$registration_key"
+  - pro attach $TOKEN
+  - landscape-config --silent --account-name="$LANDSCAPE_ACCOUNT_NAME" --computer-title="\$(hostname --long)" --url "https://$LANDSCAPE_FQDN/message-system" --ping-url "http://$LANDSCAPE_FQDN/ping" --tags="$TAGS" --script-users="$SCRIPT_USERS" --http-proxy="$HTTP_PROXY" --https-proxy="$HTTPS_PROXY" --access-group="$ACCESS_GROUP" --registration-key="$REGISTRATION_KEY"
   - pro enable livepatch
 EOF
 )
 else
     # If neither CERTBOT nor both SSL_CERTIFICATE_FILE and SSL_CERTIFICATE_KEY_FILE are set, use a self-signed SSL configuration
-cloud_init=$(cat <<EOF
+CLOUD_INIT=$(cat <<EOF
 #cloud-config
 packages:
   - ansible
@@ -98,75 +101,78 @@ runcmd:
   - systemctl stop unattended-upgrades
   - systemctl disable unattended-upgrades
   - apt-get remove -y unattended-upgrades
-  - pro attach $token
-  - echo | openssl s_client -connect $landscape_fqdn:443 | openssl x509 | tee /etc/landscape/server.pem
-  - landscape-config --silent --account-name="$landscape_account_name" --computer-title="\$(hostname --long)" --url "https://$landscape_fqdn/message-system" --ping-url "http://$landscape_fqdn/ping" --ssl-public-key=/etc/landscape/server.pem --tags="$tags" --script-users="$script_users" --http-proxy="$http_proxy" --https-proxy="$https_proxy" --access-group="$access_group" --registration-key="$registration_key"
+  - pro attach $TOKEN
+  - echo | openssl s_client -connect $LANDSCAPE_FQDN:443 | openssl x509 | tee /etc/landscape/server.pem
+  - landscape-config --silent --account-name="$LANDSCAPE_ACCOUNT_NAME" --computer-title="\$(hostname --long)" --url "https://$LANDSCAPE_FQDN/message-system" --ping-url "http://$LANDSCAPE_FQDN/ping" --ssl-public-key=/etc/landscape/server.pem --tags="$TAGS" --script-users="$SCRIPT_USERS" --http-proxy="$HTTP_PROXY" --https-proxy="$HTTPS_PROXY" --access-group="$ACCESS_GROUP" --registration-key="$REGISTRATION_KEY"
   - pro enable livepatch
 EOF
 )
 fi
 
-arch="amd64"
-lxd_virtualmachines=("jammy" "noble" "focal")
-lxd_containers=("jammy" "noble" "bionic")
-multipass_virtualmachines=("core24")
+ARCH="amd64"
+LXD_VIRTUALMACHINES=("jammy" "noble" "focal")
+LXD_CONTAINERS=("jammy" "noble" "bionic")
+MULTIPASS_VIRTUALMACHINES=("core24")
 
-declare -A lxd_virtualmachine_fingerprints
-lxd_virtualmachine_fingerprints=(
+declare -A LXD_VIRTUALMACHINE_FINGERPRINTS
+LXD_VIRTUALMACHINE_FINGERPRINTS=(
   ["focal"]="fb944b6797cf25fd4c7b8035c7e8fa0082d845032336746d94a0fb4db22bd563"
 )
 
-declare -A container_fingerprints
-container_fingerprints=(
+declare -A CONTAINER_FINGERPRINTS
+CONTAINER_FINGERPRINTS=(
   ["bionic"]="c533845b5db1747674ee915cbb20df6eb47c953bb7caf1fec5b35ae9ccf98c18"
 )
 
 # Launch Multipass instances
 
-for release in "${multipass_virtualmachines[@]}"; do
-  echo "$release virtual machine: latest"
-  multipass launch "$release" -n "$today"-"$release"
-  multipass exec "$today"-"$release" -- sudo snap install landscape-client
-  echo | openssl s_client -connect $landscape_fqdn:443 | openssl x509 | multipass transfer --parents - "$today"-"$release":/home/ubuntu/certs/landscape.pem
-  multipass exec "$today"-"$release" -- sudo cp /home/ubuntu/certs/landscape.pem /var/snap/landscape-client/common/etc/landscape.pem
-  multipass exec "$today"-"$release" -- sudo landscape-client.config --silent --account-name="$landscape_account_name" --computer-title="$today-$release" --url "https://$landscape_fqdn/message-system" --ping-url "http://$landscape_fqdn/ping" --ssl-public-key=/var/snap/landscape-client/common/etc/landscape.pem --tags="$tags" --script-users="$script_users" --http-proxy="$http_proxy" --https-proxy="$https_proxy" --access-group="$access_group" --registration-key="$registration_key"
+for RELEASE in "${MULTIPASS_VIRTUALMACHINES[@]}"; do
+  INSTANCE_NAME="$TODAY-$RELEASE"
+  echo "$RELEASE virtual machine: latest"
+  multipass launch "$RELEASE" -n "$INSTANCE_NAME"
+  multipass exec "$INSTANCE_NAME" -- sudo snap install landscape-client
+  echo | openssl s_client -connect "$LANDSCAPE_FQDN":443 | openssl x509 | multipass transfer --parents - "$INSTANCE_NAME":/home/ubuntu/certs/landscape.pem
+  multipass exec "$INSTANCE_NAME" -- sudo cp /home/ubuntu/certs/landscape.pem /var/snap/landscape-client/common/etc/landscape.pem
+  multipass exec "$INSTANCE_NAME" -- sudo landscape-config --silent --account-name="$LANDSCAPE_ACCOUNT_NAME" --computer-title="$INSTANCE_NAME" --url "https://$LANDSCAPE_FQDN/message-system" --ping-url "http://$LANDSCAPE_FQDN/ping" --ssl-public-key=/var/snap/landscape-client/common/etc/landscape.pem --tags="$TAGS" --script-users="$SCRIPT_USERS" --http-proxy="$HTTP_PROXY" --https-proxy="$HTTPS_PROXY" --access-group="$ACCESS_GROUP" --registration-key="$REGISTRATION_KEY"
 done
 
 # Launch LXD instances
 
 get_fingerprint() {
-  local release=$1
-  local type=$2
-  lxc image list ubuntu: arch=$arch release="$release" type="$type" --format yaml | \
+  local RELEASE=$1
+  local TYPE=$2
+  lxc image list ubuntu: arch=$ARCH release="$RELEASE" type="$TYPE" --format yaml | \
     yq e ".[] | .fingerprint" - | tail -1
 }
 
-for release in "${lxd_virtualmachines[@]}"; do
-  if [[ -n "${lxd_virtualmachine_fingerprints[$release]}" ]]; then
-    fingerprint=${lxd_virtualmachine_fingerprints[$release]}
+for RELEASE in "${LXD_VIRTUALMACHINES[@]}"; do
+  INSTANCE_NAME="$TODAY-$RELEASE"
+  if [[ -n "${LXD_VIRTUALMACHINE_FINGERPRINTS[$RELEASE]}" ]]; then
+    FINGERPRINT=${LXD_VIRTUALMACHINE_FINGERPRINTS[$RELEASE]}
   else
-    fingerprint=$(get_fingerprint "$release" "virtual-machine")
-    echo "Retrieved fingerprint for $release VM: $fingerprint"
+    FINGERPRINT=$(get_fingerprint "$RELEASE" "virtual-machine")
+    echo "Retrieved fingerprint for $RELEASE VM: $FINGERPRINT"
   fi
-  if [ -n "$fingerprint" ]; then
-    echo "$release VM image fingerprint: $fingerprint"
-    lxc launch ubuntu:"$fingerprint" "$today-$release-vm" --vm --config=user.user-data="$cloud_init"
+  if [ -n "$FINGERPRINT" ]; then
+    echo "$RELEASE VM image fingerprint: $FINGERPRINT"
+    lxc launch ubuntu:"$FINGERPRINT" "$INSTANCE_NAME-vm" --vm --config=user.user-data="$CLOUD_INIT"
   else
-    echo "No fingerprint found for release $release VM."
+    echo "No fingerprint found for release $RELEASE VM."
   fi
 done
 
-for release in "${lxd_containers[@]}"; do
-  if [[ -n "${container_fingerprints[$release]}" ]]; then
-    fingerprint=${container_fingerprints[$release]}
+for RELEASE in "${LXD_CONTAINERS[@]}"; do
+  INSTANCE_NAME="$TODAY-$RELEASE"
+  if [[ -n "${CONTAINER_FINGERPRINTS[$RELEASE]}" ]]; then
+    FINGERPRINT=${CONTAINER_FINGERPRINTS[$RELEASE]}
   else
-    fingerprint=$(get_fingerprint "$release" "container")
-    echo "Retrieved fingerprint for $release container: $fingerprint"
+    FINGERPRINT=$(get_fingerprint "$RELEASE" "container")
+    echo "Retrieved fingerprint for $RELEASE container: $FINGERPRINT"
   fi
-  if [ -n "$fingerprint" ]; then
-    echo "$release container image fingerprint: $fingerprint"
-    lxc launch ubuntu:"$fingerprint" "$today-$release-c" --config=user.user-data="$cloud_init"
+  if [ -n "$FINGERPRINT" ]; then
+    echo "$RELEASE container image fingerprint: $FINGERPRINT"
+    lxc launch ubuntu:"$FINGERPRINT" "$INSTANCE_NAME-c" --config=user.user-data="$CLOUD_INIT"
   else
-    echo "No fingerprint found for release $release container."
+    echo "No fingerprint found for release $RELEASE container."
   fi
 done
