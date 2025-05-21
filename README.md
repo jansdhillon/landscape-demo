04c93068e23de13bf9f21f079bb76e941906b7a3
Jan <77344313+jansdhillon@users.noreply.github.com>
2025-05-21 15:55:26 -0600

    update script, move controller logic into script, update readme to mention script execution/logging in

--- README.md
+++ README.md
@@ -25,12 +25,6 @@ sudo snap install juju --classic
 - 💡 **TIP**: Make sure LXD has been initialized before proceeding. You should see `lxd` when running `groups`, otherwise see [the LXD documentation](https://documentation.ubuntu.com/lxd) to get set up.
 
 
-Now, create a local LXD cloud with Juju, which will allow us to easily orchestrate the lifecycle of our Landscape system:
-
-```bash
-juju bootstrap lxd landscape-controller
-```
-
 ## Create the Ubuntu instances for Landscape
 
 We need an Ubuntu Pro token to use Landscape, which we can get [here](https://ubuntu.com/pro/dashboard). Save the token value to the `PRO_TOKEN` environment variable:
@@ -39,9 +33,36 @@ We need an Ubuntu Pro token to use Landscape, which we can get [here](https://ub
 export PRO_TOKEN=... # your token here
 ```
 
-[./run.sh](run.sh) will create Ubuntu instances to run Landscape, starting with Landscape Server and other applications it depends on, followed by Landscape Client instances that are  managed by Landscape Server.
+[./run.sh](run.sh) will create Ubuntu instances to run Landscape, starting with Landscape Server and other applications it depends on, followed by Landscape Client instances that are managed by Landscape Server.
 
 - 💡 **TIP**: Use `juju status --watch 2s` for a live view of the Juju model's lifecycle.
+
+
+## Logging in 
+
+The first administrator account is created for you, and the credentials are as follows:
+
+- Email: `admin@example.com`
+- Password: `pwd`
+
+## Script Execution
+
+[./run.sh](run.sh) created a script and a script profile for it, which makes it execute on the Landscape Client instance on a set interval:
+
+```bash
+#!/bin/bash
+echo "Hello world!" | tee hello.txt'
+```
+
+Additionally, in the [Activities tab](https://landscape.example.com/new_dashboard/activities), you can see that it was already (or set to be) manually executed on the Landscape Client instance.
+
+After the script has finished running, we can verify the script ran by SSH'ing into the Landscape Client unit:
+
+```bash
+juju ssh landscape-client/0
+cat hello.txt
+# Hello world!
+```
 
 ## Tearing Down and Cleaning Up
 
@@ -54,6 +75,4 @@ HAPROXY_IP=$(juju show-unit haproxy/0 | yq '."haproxy/0".public-address')
 sudo sed -i "/${HAPROXY_IP}[[:space:]]\\+landscape\.example\.com/d" /etc/hosts
 # Destroy our controller and model
 juju destroy-controller --no-prompt landscape-controller --destroy-all-models --no-wait --force
-# Remove server cert file
-rm server.pem
 ```
--- run.sh
+++ run.sh
@@ -19,41 +19,47 @@ EOF
 
 LANDSCAPE_FQDN="landscape.example.com"
 MODEL_NAME="landscape"
+CONTROLLER_NAME="landscape-controller"
 REGISTRATION_KEY="key"
 # fka "series"
 PPA="ppa:landscape/self-hosted-beta"
 NOW=$(date -u +"%Y-%m-%dT%H:%M:%S")
 # Landsacpe Client units
-NUM_LS_CLIENT_UNITS=3
+NUM_LS_CLIENT_UNITS=1
 CLIENT_BASE="ubuntu@20.04"
 SERVER_BASE="ubuntu@22.04"
 # Postgres units
-NUM_DB_UNITS=2
+NUM_DB_UNITS=1
+ADMIN_EMAIL="admin@example.com"
+ADMIN_PASSWORD="pwd"
+ADMIN_NAME="Landscape Admin"
 
 while [[ -z $PRO_TOKEN ]]; do
-  echo -n "'PRO_TOKEN' is not set. Visit https://ubuntu.com/pro/dashboard to get it,"
+  printf "'PRO_TOKEN' is not set. Visit https://ubuntu.com/pro/dashboard to get it,"
   read -r -p " and enter it here: " PRO_TOKEN
 done
 
 cleanup() {
-  echo "Cleaning up model and exiting..."
+  printf "Cleaning up model and exiting...\n"
   rm -rf server.pem
 
-  if [ -n "$HAPROXY_IP" ] && [ -n "$LANDSCAPE_FQDN" ]; then
-    echo "Modifying /etc/hosts requires elevated privileges."
-    sudo sed -i.bak "/$HAPROXY_IP[[:space:]]\+$LANDSCAPE_FQDN/d" /etc/hosts
+  if [ -n "${HAPROXY_IP}" ] && [ -n "${LANDSCAPE_FQDN}" ]; then
+    printf "Modifying /etc/hosts requires elevated privileges.\n"
+    sudo sed -i.bak "/${HAPROXY_IP}[[:space:]]\+${LANDSCAPE_FQDN}/d" /etc/hosts
   fi
 
-  juju destroy-model --no-prompt $MODEL_NAME --no-wait --force
+  juju destroy-controller --no-prompt "${CONTROLLER_NAME}" --destroy-all-models --no-wait --force
   exit
 }
 
 trap cleanup SIGINT
 trap cleanup ERR
 
-juju add-model $MODEL_NAME
+juju bootstrap lxd "${CONTROLLER_NAME}"
 
-echo "Provisioning machines..."
+juju add-model "${MODEL_NAME}"
+
+printf "Provisioning machines...\n"
 
 # Add the Landscape Server unit and the other apps we need to run Landscape
 # based on https://github.com/canonical/landscape-bundles/blob/scalable-stable/bundle.yaml
@@ -61,8 +67,12 @@ echo "Provisioning machines..."
 juju deploy ch:landscape-server \
   --config landscape_ppa="${PPA}" \
   --constraints mem=4096 \
-  --base "$SERVER_BASE" \
-  --config registration_key="${REGISTRATION_KEY}"
+  --base "${SERVER_BASE}" \
+  --config registration_key="${REGISTRATION_KEY}" \
+  --config admin_name="${ADMIN_NAME}" \
+  --config admin_email="${ADMIN_EMAIL}" \
+  --config admin_password="${ADMIN_PASSWORD}" \
+  --config min_install=true
 
 juju deploy ch:haproxy \
   --channel stable \
@@ -86,7 +96,7 @@ juju deploy ch:postgresql \
   --revision 468 \
   --base ubuntu@22.04 \
   --constraints mem=2048 \
-  -n "$NUM_DB_UNITS"
+  -n "${NUM_DB_UNITS}"
 
 juju deploy ch:rabbitmq-server \
   --channel 3.9/stable \
@@ -95,111 +105,89 @@ juju deploy ch:rabbitmq-server \
   --config consumer-timeout=259200000
 
 # For Landscape Client to use in the future
-juju deploy --base $CLIENT_BASE lxd -n $NUM_LS_CLIENT_UNITS
-
-echo "Waiting for LXD units to become active..."
-juju wait-for application lxd --query='(status=="active")'
-
-echo "Attaching Ubuntu Pro token..."
-for i in $(seq 0 $((NUM_LS_CLIENT_UNITS - 1))); do
-  echo "Attaching token to lxd/${i}"
-  juju ssh "lxd/${i}" "sudo pro attach ${PRO_TOKEN}"
-done
+juju deploy --base "${CLIENT_BASE}" lxd -n "${NUM_LS_CLIENT_UNITS}"
 
 # Next, setup the relations
 
-juju relate landscape-server rabbitmq-server
-juju relate landscape-server haproxy
+juju integrate landscape-server rabbitmq-server
+juju integrate landscape-server haproxy
 juju integrate landscape-server:db postgresql:db-admin
 
-echo "Waiting for the model to settle"
-while true; do
-  ls_status=$(juju status landscape-server --format=yaml | yq '.applications.landscape-server.application-status.current')
-  pg_status=$(juju status postgresql --format=yaml | yq '.applications.postgresql.application-status.current')
-
-  if [[ "$ls_status" == "active" && "$pg_status" == "active" ]]; then
-    echo "done."
-    break
-  fi
-  printf "."
-  sleep 1
-done
+printf "Waiting for the model to settle...\n"
+juju wait-for model "${MODEL_NAME}" --timeout 3600s --query='forEach(units, unit => unit.workload-status == "active")'
 
 # Get the HAProxy IP
 
 HAPROXY_IP=$(juju show-unit "haproxy/0" | yq '."haproxy/0".public-address')
-echo "$HAPROXY_IP $LANDSCAPE_FQDN" | sudo tee -a /etc/hosts >/dev/null
-
-# Get the self-signed cert
-echo | openssl s_client -connect "${HAPROXY_IP}:443" | openssl x509 | sudo tee server.pem >/dev/null
-
-# base64 encode it to use for Landscape Client units
-
-B64_CERT=$(cat server.pem | base64)
-
-echo "Visit https://${LANDSCAPE_FQDN} to finalize Landscape Server configuration,"
-read -r -p "then press Enter to continue provisioning Landscape Client instances, or CTRL+C to exit..."
-
-# Deploy Landscape Client
-
-juju deploy ch:landscape-client --config account-name='standalone' \
-  --config ping-url="http://${HAPROXY_IP}/ping" \
-  --config url="https://${HAPROXY_IP}/message-system" \
-  --config ssl-public-key="base64:${B64_CERT}" \
-  --config ppa="${PPA}" \
-  --config registration-key="${REGISTRATION_KEY}" \
-  --config script-users="ALL" \
-  --config include-manager-plugins="ScriptExecution"
-
-# Relate it to LXD
-
-juju relate lxd landscape-client
-
-# Get the admin username and credentials
-
-echo "The credentials from the first Landscape user you just created are needed to perform actions like creating scripts."
+printf "%s %s" "$HAPROXY_IP" "$LANDSCAPE_FQDN" | sudo tee -a /etc/hosts >/dev/null
 
 while true; do
-  read -r -p "Enter the email of the user you just created: " ADMIN_EMAIL
-  read -r -s -p "Enter the password: " ADMIN_PASSWORD
-  echo
+  # Get the self-signed cert
+  set +e
+  B64_CERT=$(
+    echo | openssl s_client -connect "$HAPROXY_IP:443" 2>/dev/null | openssl x509 2>/dev/null | base64
+  )
+  set -e
 
-  RESPONSE=$(curl -skX POST "https://${LANDSCAPE_FQDN}/api/v2/login" \
+  if [ "${B64_CERT}" != "null" ] && [ -n "${B64_CERT}" ]; then
+    break
+  else
+    printf "Failed to get certificate.\n"
+    printf "Trying again... (CTRL+C to abort)\n"
+    sleep 5
+  fi
+done
+
+# Get the JWT
+# We do this in a loop to avoid transient 503s
+while true; do
+  login_response=$(curl -skX POST "https://${LANDSCAPE_FQDN}/api/v2/login" \
     -H "Content-Type: application/json" \
     -d "{\"email\": \"${ADMIN_EMAIL}\", \"password\": \"${ADMIN_PASSWORD}\"}")
 
-  JWT=$(echo "${RESPONSE}" | yq -r '.token')
+  JWT=$(printf "%s" $login_response | yq -r '.token')
 
   if [ "${JWT}" != "null" ] && [ -n "${JWT}" ]; then
-    echo "Login successful\!"
+    printf "Login successful\!\n"
     break
   else
-    echo "Login failed. Try again? (y/n)"
-    read -r RETRY
-    if [[ "${RETRY}" != "y" && "${RETRY}" != "Y" ]]; then
-      echo "Exiting."
-      exit 1
-    fi
+    printf "Login failed. Response: %s\n" "$login_response"
+    printf "Trying again... (CTRL+C to abort)\n"
+    sleep 5
   fi
 done
 
 make_rest_api_request() {
-  local url=$1
-  local method=$2
+  local method=$1
+  local url=$2
   local body=$3
 
   if [[ -n "$body" ]]; then
-    RESPONSE=$(curl -skX "$method" "$url" \
-      -H "Authorization: Bearer $JWT" \
+    response=$(curl -skX "${method}" "${url}" \
+      -H "Authorization: Bearer ${JWT}" \
       -H "Content-Type: application/json" \
-      -d "$body")
+      -d "${body}")
   else
-    RESPONSE=$(curl -skX "${method}" "$url" \
-      -H "Authorization: Bearer $JWT")
+    response=$(curl -skX "${method}" "${url}" \
+      -H "Authorization: Bearer ${JWT}")
   fi
 
-  echo "Response: $RESPONSE" | yq
+  printf "Response: %s\n" "$response" | yq
 }
+
+# enable auto registration
+
+SET_PREFERENCES_URL="https://${LANDSCAPE_FQDN}/api/v2/preferences"
+
+BODY=$(
+  cat <<EOF
+{
+  "auto_register_new_computers": true
+}
+EOF
+)
+
+make_rest_api_request "PATCH" "${SET_PREFERENCES_URL}" "${BODY}"
 
 # Create a script
 
@@ -208,7 +196,7 @@ echo "Hello world!" | tee hello.txt')
 
 CREATE_SCRIPT_URL="https://${LANDSCAPE_FQDN}/api?action=CreateScript&version=2011-08-01&code=${EXAMPLE_CODE}&title=Test+Script&script_type=V2&access_group=global"
 
-make_rest_api_request "$CREATE_SCRIPT_URL" "GET"
+make_rest_api_request "GET" "${CREATE_SCRIPT_URL}"
 
 # Create a script profile
 
@@ -225,21 +213,40 @@ BODY=$(
   "trigger": {
     "trigger_type": "recurring",
     "interval": "* * * * *",
-    "start_after": "$NOW"
+    "start_after": "${NOW}"
   },
   "username": "root"
 }
 EOF
 )
 
-make_rest_api_request "$CREATE_SCRIPT_PROFILE_URL" "POST" "$BODY"
+make_rest_api_request "POST" "${CREATE_SCRIPT_PROFILE_URL}" "${BODY}"
 
-juju wait-for application landscape-client --query='(status=="active")'
+# Deploy Landscape Client
+
+juju deploy ch:landscape-client --config account-name='standalone' \
+  --config ping-url="http://${HAPROXY_IP}/ping" \
+  --config url="https://${HAPROXY_IP}/message-system" \
+  --config ssl-public-key="base64:${B64_CERT}" \
+  --config ppa="${PPA}" \
+  --config registration-key="${REGISTRATION_KEY}" \
+  --config script-users="ALL" \
+  --config include-manager-plugins="ScriptExecution"
+
+printf "Attaching Ubuntu Pro token...\n"
+for i in $(seq 0 $((NUM_LS_CLIENT_UNITS - 1))); do
+  printf "Attaching token to lxd/${i}\n"
+  juju ssh "lxd/${i}" "sudo pro attach ${PRO_TOKEN}"
+done
+
+juju integrate lxd landscape-client
+
+juju wait-for model "${MODEL_NAME}" --timeout 3600s --query='forEach(units, unit => unit.workload-status == "active")'
 
 # Manually execute the script on the Landscape Client instances
 
-EXECUTE_SCRIPT_URL="https://${LANDSCAPE_FQDN}/api/?action=ExecuteScript&version=2011-08-01&query=id:2+OR+id:1&script_id=1&username=root&time_limit=300"
+EXECUTE_SCRIPT_URL="https://${LANDSCAPE_FQDN}/api/?action=ExecuteScript&version=2011-08-01&query=id:1&script_id=1&username=root&time_limit=300"
 
-make_rest_api_request "$EXECUTE_SCRIPT_URL" "GET"
+make_rest_api_request "GET" "${EXECUTE_SCRIPT_URL}"
 
-echo "Setup complete! Login at https://${LANDSCAPE_FQDN}/new_dashboard to start managing your Ubuntu fleet."
+printf "Setup complete\!\nYou can now login at https://%s/new_dashboard using the following credentials:\nEmail: %s\nPassword: %s\n" "$LANDSCAPE_FQDN" "$ADMIN_EMAIL" "$ADMIN_PASSWORD"
