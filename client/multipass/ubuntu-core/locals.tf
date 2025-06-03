f6dc5c27a2964cd5e732fd4aae12beeed227da66
jansdhillon <jan.dhillon@canonical.com>
2025-06-02 23:07:56 -0600

    rm changes to ubuntu core

--- client/multipass/ubuntu-core/locals.tf
+++ client/multipass/ubuntu-core/locals.tf
@@ -1,29 +1,14 @@
 locals {
   landscape_client_cloud_init = <<-EOF
-    #cloud-config
-    package_update: false
-    packages: []
-    runcmd:
-      - snap refresh snapd
-      - systemctl stop unattended-upgrades
-      - systemctl disable unattended-upgrades
-      - mkdir -p /var/snap/landscape-client/common/etc/
-      - pro attach ${var.pro_token}
-      - echo | openssl s_client -connect "${var.landscape_fqdn}:443" | openssl x509 | sudo tee /var/snap/landscape-client/common/etc/server.pem
-      - |
-        snap install landscape-client
-
-        until snap services | grep -q "landscape-client.*active"; do
-          echo "Waiting for landscape-client service to be active..."
-          sleep 5
-        done
-
-        landscape-client.config --silent \
-          --account-name="${var.landscape_account_name}" \
-          --computer-title="$(hostname --long)" \
-          --url "https://${var.landscape_fqdn}/message-system" \
-          --ping-url "http://${var.landscape_fqdn}/ping" \
-          --registration-key="${var.registration_key}" \
-          --ssl-public-key="/var/snap/landscape-client/common/etc/server.pem"
-  EOF
+#cloud-config
+package_update: false
+packages: []
+runcmd:
+  - snap refresh snapd
+  - systemctl stop unattended-upgrades
+  - systemctl disable unattended-upgrades
+  - mkdir -p /var/snap/landscape-client/common/etc/
+  - echo | openssl s_client -connect "${var.landscape_fqdn}:443" | openssl x509 | sudo tee /var/snap/landscape-client/common/etc/server.pem
+  - snap install landscape-client && landscape-client.config --silent --account-name="${var.landscape_account_name}" --computer-title="$(hostname --long)" --url "https://${var.landscape_fqdn}/message-system" --ping-url "http://${var.landscape_fqdn}/ping" --registration-key="${var.registration_key}" --ssl-public-key="/var/snap/landscape-client/common/etc/server.pem"
+EOF
 }
--- client/multipass/ubuntu-core/main.tf
+++ client/multipass/ubuntu-core/main.tf
@@ -8,13 +8,4 @@ resource "multipass_instance" "inst2" {
   cpus           = 1
   image          = var.ubuntu_core_series
   cloudinit_file = local_file.cloud_init_user_data.filename
-
-  provisioner "local-exec" {
-    command = <<-EOT
-      multipass exec "$DEVICE_NAME" --verbose -- cloud-init status --wait
-    EOT
-    environment = {
-      DEVICE_NAME = var.device_name
-    }
-  }
 }
