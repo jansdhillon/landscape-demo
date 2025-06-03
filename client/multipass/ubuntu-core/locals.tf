4c2d253de2c28bf8f78ae89ff8ead2a51a257d5f
jansdhillon <jan.dhillon@canonical.com>
2025-06-03 00:06:42 -0600

    use snap run

--- client/multipass/ubuntu-core/locals.tf
+++ client/multipass/ubuntu-core/locals.tf
@@ -9,7 +9,6 @@ runcmd:
   - systemctl disable unattended-upgrades
   - mkdir -p /var/snap/landscape-client/common/etc/
   - echo | openssl s_client -connect "${var.landscape_fqdn}:443" | openssl x509 | sudo tee /var/snap/landscape-client/common/etc/server.pem
-  - snap install landscape-client
-  - timeout 300 bash -c 'until snap services | grep -q "landscape-client.*active"; do printf "."; sleep 5; done' && landscape-client.config --silent --account-name="${var.landscape_account_name}" --computer-title="$(hostname --long)" --url "https://${var.landscape_fqdn}/message-system" --ping-url "http://${var.landscape_fqdn}/ping" --registration-key="${var.registration_key}" --ssl-public-key="/var/snap/landscape-client/common/etc/server.pem"
+  - snap install landscape-client && snap run landscape-client.config --silent --account-name="${var.landscape_account_name}" --computer-title="$(hostname --long)" --url "https://${var.landscape_fqdn}/message-system" --ping-url "http://${var.landscape_fqdn}/ping" --registration-key="${var.registration_key}" --ssl-public-key="/var/snap/landscape-client/common/etc/server.pem"
 EOF
 }
